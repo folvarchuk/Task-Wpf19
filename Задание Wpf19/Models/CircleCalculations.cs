ad2b7c0acd823be05506d4a4f7175e70cade7393
Андрей Фольварчук <folvarchuk@mail.ru>
2022-02-18 20:09:21 +0300

    Добавьте файлы проекта.

---
+++ Задание Wpf19/Models/CircleCalculations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_Wpf19.Models
+{
+    static class CircleCalculations
+    {
+        public static double CircleLengthCalc(double r) => 2 * Math.PI * r;
+    }
+}
---
+++ Задание Wpf19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Задание_Wpf19.Models;
+
+namespace Задание_Wpf19.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private double result;
+        public double Result
+        {
+            get => result;
+            set
+            {
+                result = value;
+            }
+        }
+        
+        public ICommand  CircleLengthCalcCommand { get; }
+        private void OnCircleLengthCalcCommand(object p)
+        {
+            Result = CircleCalculations.CircleLengthCalc(Radius);
+        }
+
+        private bool CanCircleLengthCalcCommandExecuted(object p)
+        {
+            if (Radius != 0)
+                return true;
+            else
+                return false;
+        }
+
+        public MainWindowViewModel()
+        {
+            CircleLengthCalcCommand = new RelayCommand(OnCircleLengthCalcCommand, CanCircleLengthCalcCommandExecuted);
+        }
+    }
+}
